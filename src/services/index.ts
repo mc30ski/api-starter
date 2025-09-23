92e6f429175cd6d0988fb3a2ab79fe329f868451
Michael Wanderski <mike@wanderski.com>
2025-09-23 17:07:41 -0400

    working

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,6 @@
 import { prisma } from "../db/prisma";
 import type { Services } from "../types/services";
-//import { Prisma, PrismaClient, EquipmentStatus } from "@prisma/client";
+import { Prisma, PrismaClient, EquipmentStatus } from "@prisma/client";
 
 export const services: Services = {
   equipment: {
