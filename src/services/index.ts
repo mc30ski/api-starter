1c43331f3c0fbed9e3174e0df2f9c1d7b07792af
Michael Wanderski <mike@wanderski.com>
2025-09-23 16:48:57 -0400

    working

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,9 +1,11 @@
+import { prisma } from "../db/prisma";
 import type { Services } from "../types/services";
+//import { Prisma, PrismaClient, EquipmentStatus } from "@prisma/client";
 
 export const services: Services = {
   equipment: {
     async listAvailable() {
-      return [{ name: "mower", description: "gas powered" }];
+      return [];
     },
   },
 };
