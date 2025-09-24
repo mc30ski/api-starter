4fede110efd61de1ebfc7151fb27a2faee87bdee
Michael Wanderski <mike@wanderski.com>
2025-09-24 09:26:38 -0400

    working

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -4,9 +4,9 @@ import { services } from "../services";
 export default async function adminRoutes(app: FastifyInstance) {
   //TODO:
   const adminGuard = {
-    //  preHandler: [
-    //    (app as any).requireRole.bind(app, undefined, undefined, "ADMIN"),
-    //  ],
+    preHandler: [
+      (app as any).requireRole.bind(app, undefined, undefined, "ADMIN"),
+    ],
   };
 
   app.get("/admin/equipment", adminGuard, async () =>
